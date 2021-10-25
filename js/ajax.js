f07237292dec2eb7f5c379ee5cffb78878c8c3df
Daniel Bernal <danberjim13@hotmail.com>
2021-10-25 17:47:33 -0500

    primer commit

---
+++ js/ajax.js
@@ -0,0 +1,36 @@
+export function contact () {
+    const AJAX = new Promise((resolve, reject) =>{
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "contactame.html");
+        xhr.addEventListener("load", (e)=>{
+            if (xhr.status == 200) {
+                resolve(e.target.response);
+            } else {
+                reject("Estamos presentando un inconveniente, porfavor intentalo más tarde")
+            }
+        })
+        xhr.send();
+    })
+    AJAX.then (res =>{
+        contenedor.innerHTML = res;
+    });
+    AJAX.catch (err => alert(err));
+}
+export function creaciones() {
+    const AJAX = new Promise((resolve, reject) =>{
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "portafolio.html");
+        xhr.addEventListener("load", (e)=>{
+            if (xhr.status == 200) {
+                resolve(e.target.response);
+            } else {
+                reject("Estamos presentando un inconveniente, porfavor intentalo más tarde")
+            }
+        })
+        xhr.send();
+    })
+    AJAX.then (res =>{
+        contenedor.innerHTML = res;
+    });
+    AJAX.catch (err => alert(err));    
+}
---
+++ js/app.js
@@ -0,0 +1,40 @@
+import { contact } from "./ajax.js";
+import { creaciones } from "./ajax.js"
+let contenedor = document.getElementById("contenedor");
+let main = document.querySelector("main");
+let contactame = document.getElementById("contactame");
+contactame.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    main.className = "hidden";
+    contenedor.className = "";
+    contact();
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
+})
+
+let home = document.getElementById("home").addEventListener("click", ()=>{
+    main.className = "";
+    contenedor.className = "hidden";
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
+})
+
+let portafolio = document.getElementById("portafolio");
+portafolio.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    main.className = "hidden";
+    contenedor.className = "";
+    creaciones();
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
+})
+// let body = document.querySelector("body")
+let nav = document.querySelector("nav");
+let enlaces = document.querySelector(".enlaces-menu");
+let hamburguer = document.getElementById("hamburguer");
+hamburguer.addEventListener("click",(e)=>{
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
+})
