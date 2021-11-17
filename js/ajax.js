a3d265164054815b19b3815c7f5fe8930fb865b4
Daniel Bernal <danberjim13@hotmail.com>
2021-11-17 18:07:18 -0500

    Se crea la section mapa y se generan los estilos de la misma

--- js/ajax.js
+++ js/ajax.js
@@ -34,3 +34,21 @@ export function creaciones() {
     });
     AJAX.catch (err => alert(err));    
 }
+export function mapa() {
+    const AJAX = new Promise((resolve, reject) =>{
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "mapa.html");
+        xhr.addEventListener("load", (e) =>{
+            if(xhr.status === 200) {
+                resolve(e.target.response);
+            }else{
+                reject("no se puede cargar la página en éste momento, disculpe las molestias");
+            }
+        })
+        xhr.send()
+    })
+    AJAX.then (res =>{
+        contenedor.innerHTML = res;
+    })
+    AJAX.catch(err => alert (err));
+}
--- js/app.js
+++ js/app.js
@@ -1,37 +1,65 @@
-import { contact } from "./ajax.js";
+import { contact, mapa } from "./ajax.js";
 import { creaciones } from "./ajax.js";
 let contenedor = document.getElementById("contenedor");
 let main = document.querySelector("main");
 let contactame = document.getElementById("contactame");
 contactame.addEventListener("click", (e) => {
-    e.stopPropagation();
     e.preventDefault();
     main.className = "hidden";
     contenedor.className = "";
     contact();
     nav.classList.toggle("activo");
     enlaces.classList.toggle("activado");
-})
-let home = document.getElementById("home").addEventListener("click", ()=>{
+});
+let home = document.getElementById("home");
+let profession = document.getElementById("profession");
+home.addEventListener("click", (e)=>{
+    e.preventDefault();
     main.className = "";
     contenedor.className = "hidden";
     nav.classList.toggle("activo");
     enlaces.classList.toggle("activado");
-})
+});
+profession.addEventListener("click", (e)=>{
+    e.preventDefault();
+    main.className = "";
+    contenedor.className = "hidden";
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
+});
 let portafolio = document.getElementById("portafolio");
 portafolio.addEventListener("click", (e) => {
-    e.stopPropagation();
     e.preventDefault();
     main.className = "hidden";
     contenedor.className = "";
     creaciones();
     nav.classList.toggle("activo");
     enlaces.classList.toggle("activado");
+});
+let barrio = document.getElementById("mapa");
+barrio.addEventListener("click", (e) =>{
+    e.preventDefault();
+    main.className = "hidden";
+    contenedor.className = "";
+    mapa();
+    nav.classList.toggle("activo");
+    enlaces.classList.toggle("activado");
 })
+//menú hamburguesa
 let nav = document.querySelector("nav");
 let enlaces = document.querySelector(".enlaces-menu");
 let hamburguer = document.getElementById("hamburguer");
 hamburguer.addEventListener("click",(e)=>{
     nav.classList.toggle("activo");
     enlaces.classList.toggle("activado");
+})
+//animación en menú
+const menu = document.querySelectorAll("nav ul li");
+menu.forEach((e)=>{
+    e.addEventListener("mouseover", function(e) {
+        e.target.classList.add("signal");
+        setTimeout(() => {
+            e.target.classList.remove("signal");
+        }, 1000);
+    },false)
 })
